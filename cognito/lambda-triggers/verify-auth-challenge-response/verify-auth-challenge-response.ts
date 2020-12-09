d0d45816822cf736782c3508dbec775a5a87341d
Nolan330 <nolan@nuid.io>
2020-12-09 09:46:03 -0500

    Update sign in/authentication process

--- cognito/lambda-triggers/verify-auth-challenge-response/verify-auth-challenge-response.ts
+++ cognito/lambda-triggers/verify-auth-challenge-response/verify-auth-challenge-response.ts
@@ -2,13 +2,20 @@
 // SPDX-License-Identifier: MIT-0
 
 import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
+import Zk from '@nuid/zk';
 
 export const handler: CognitoUserPoolTriggerHandler = async event => {
-    const expectedAnswer = event.request.privateChallengeParameters!.secretLoginCode; 
-    if (event.request.challengeAnswer === expectedAnswer) {
-        event.response.answerCorrect = true;
-    } else {
-        event.response.answerCorrect = false;
-    }
-    return event;
+  const proof = JSON.parse(event.request.challengeAnswer);
+  const challenge = JSON.parse(event.request.privateChallengeParameters.challenge);
+
+  // Combine the client-generated, single-use zero knowledge proof with the
+  // challenge generated in `create-auth-challenge` to produce data that can be
+  // verified as with `Zk.isVerified`.
+  const verifiable = Zk.verifiableFromProofAndChallenge(proof, challenge);
+
+  // The success of the authentication attempt is simply the result of verifying
+  // the combined proof and challenge
+  event.response.answerCorrect = Zk.isVerified(verifiable);
+
+  return event;
 };
