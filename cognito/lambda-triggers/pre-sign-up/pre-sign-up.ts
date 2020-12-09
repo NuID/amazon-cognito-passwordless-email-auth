7ef8f01c0aa3eb2f842315a41df1380556c38dfa
Nolan330 <nolan@nuid.io>
2020-12-09 09:44:52 -0500

    Update sign up/registration process

--- cognito/lambda-triggers/pre-sign-up/pre-sign-up.ts
+++ cognito/lambda-triggers/pre-sign-up/pre-sign-up.ts
@@ -2,8 +2,18 @@
 // SPDX-License-Identifier: MIT-0
 
 import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
+import Zk from '@nuid/zk';
 
 export const handler: CognitoUserPoolTriggerHandler = async event => {
+    const verifiable = JSON.parse(event.request.validationData.verifiable);
+
+    // Verify the "self-signed" proof in order to verify that a well-formed
+    // proof has been generated upfront, prior to continuing with registration
+    // or persisting new user data. This isn't strictly necessary but is
+    // considered good practice.
+    if (!verifiable || !Zk.isVerified(verifiable))
+      throw "Invalid request"
+
     event.response.autoConfirmUser = true;
     return event;
 };
